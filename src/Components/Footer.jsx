4b263ec119c56c53fa5760c5b1f86e689fa232d2
Athif Binu <binuathif@gmail.com>
2023-03-22 23:53:22 +0530

    footer conpleat

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,17 +1,60 @@
 import React from 'react'
 import "../Styles/Footer.css"
+import {motion} from 'framer-motion'
 const Footer = () => {
   return (
     <footer className='footer'>
-        <div className="container">
 
             <div className="next-btn">
                <button>1</button>
                <button>2</button>
                <p>Next</p>
+                  
+            </div>
+            <div className="footer-img">
+               <motion.img whileHover={{scale:0.9}} src="https://images.unsplash.com/photo-1554663565-f60b88adfa08?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80" alt="" />
+               <motion.img whileHover={{scale:0.9}} src="https://images.unsplash.com/photo-1573865526739-10659fec78a5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=415&q=80" alt="" />
+               <motion.img whileHover={{scale:0.9}} src="https://images.unsplash.com/photo-1558981403-c5f9899a28bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" alt="" />
+               <motion.img whileHover={{scale:0.9}} src="https://images.unsplash.com/photo-1541873676-a18131494184?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=418&q=80" alt="" />
+               <motion.img whileHover={{scale:0.9}} src="https://images.unsplash.com/photo-1614728263952-84ea256f9679?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1008&q=80" alt="" />
+               <motion.img whileHover={{scale:0.9}} src="https://images.unsplash.com/photo-1511436389635-11134775c795?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" alt="" />
             </div>
 
-        </div>
+            <div className="folow-card">
+            
+              <h3>Follow My Instagram</h3>
+                 <p>@logoipsum</p>
+              
+            </div>
+
+            <div className="fooer-icon">
+              <div className="icon-section">
+                  <div class="rectangles"></div>
+                  <div class="circles"> </div>
+                   <div class="circles"> </div>
+              </div>
+                 <h2>logoipsum</h2>
+
+            </div>
+
+
+            <div className="social-text">
+                    <div className="text-box">
+                      
+                        <ul>
+                          <li>About ME</li>
+                          <li>CONTACT ME</li>
+                          <li>ADVERTISING</li>
+                          <li>TERMS OF USE</li>
+                          <li>SUBSCRIBE</li>
+                        </ul>
+                        
+                    </div>
+                 </div>
+
+            
+
+      
     </footer>
   )
 }
--- src/Pages/Popular.jsx
+++ src/Pages/Popular.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import "../Styles/popular.css"
-
+import {motion} from "framer-motion"
 import {AiOutlineSearch} from "react-icons/ai"
 
 const Popular = () => {
@@ -13,7 +13,7 @@ const Popular = () => {
 
           <div className="card-section">
             <div className="card">
-                <img src="https://images.unsplash.com/photo-1583847268964-b28dc8f51f92?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80" alt="" />
+                <motion.img  whileHover={{scale:0.9}} src="https://images.unsplash.com/photo-1583847268964-b28dc8f51f92?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80" alt="" />
                  <p>Travel</p>
                  <h3>sheets containing pages</h3>
                  <br />
@@ -21,7 +21,7 @@ const Popular = () => {
             </div>
 
             <div className="card">
-                <img src="https://images.unsplash.com/photo-1625054790108-6a5fb0c174af?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80" alt="" />
+                <motion.img whileHover={{scale:0.9}} src="https://images.unsplash.com/photo-1625054790108-6a5fb0c174af?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80" alt="" />
                  <p>Travel</p>
                  <h3>sheets containing pages</h3>
                  <br />
@@ -29,7 +29,7 @@ const Popular = () => {
             </div>
 
             <div className="card">
-                <img src="https://images.unsplash.com/photo-1549082984-1323b94df9a6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" alt="" />
+                <motion.img whileHover={{scale:0.9}} src="https://images.unsplash.com/photo-1549082984-1323b94df9a6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" alt="" />
                  <p>Travel</p>
                  <h3>sheets containing pages</h3>
                  <br />
@@ -37,7 +37,7 @@ const Popular = () => {
             </div>
 
             <div className="card">
-                <img src="https://images.unsplash.com/photo-1633356122544-f134324a6cee?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" alt="" />
+                <motion.img whileHover={{scale:0.9}} src="https://images.unsplash.com/photo-1633356122544-f134324a6cee?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" alt="" />
                  <p>Travel</p>
                  <h3>sheets containing pages</h3>
                  <br />
@@ -62,7 +62,7 @@ const Popular = () => {
 
                <div className="profile-section">
         
-                     <img src="https://images.unsplash.com/photo-1544005313-94ddf0286df2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=388&q=80" alt="" />
+                     <motion.img whileHover={{scale:0.9}} src="https://images.unsplash.com/photo-1544005313-94ddf0286df2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=388&q=80" alt="" />
                      <h1>Your Name</h1>
                     <p>Brief description of yourself</p>
                </div>
